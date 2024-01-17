dbf25429dd835a6c1a104ed5a659258dbc221794
JACKRUNDLE <rundlej@oregonstate.edu>
2024-01-17 14:06:19 -0800

    new update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello from rundlej
+          Automatically Deployed by Github for rundlej.
         </p>
         <a
           className="App-link"
